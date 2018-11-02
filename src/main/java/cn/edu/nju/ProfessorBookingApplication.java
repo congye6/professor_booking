45a5ef9bf69ca81bb4d998c06d7567f07b7f21b4
congye6 <244053679@qq.com>
2018-11-02 21:19:19 +0800

    start

---
+++ src/main/java/cn/edu/nju/ProfessorBookingApplication.java
@@ -0,0 +1,12 @@
+package cn.edu.nju;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfessorBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfessorBookingApplication.class, args);
+	}
+}
